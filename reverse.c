8e768ae2ccb9a99959aa45bf7f1abcbe24515362
giftcup <salometambe@gmail.com>
2021-07-16 23:45:18 +0100

    Reverses the inputs of an array

---
+++ reverse.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    int *temp = 0;
+    temp = (int*)malloc(num * sizeof(int));
+    for (int j = 0; j < num; j++) {
+        *(temp + j) = *(arr + j);
+    }
+    
+    // Write the logic to reverse the array.
+ 
+    for(int i = 0; i < num; i++){
+        *(arr + i) = *(temp + num - i - 1);
+    }
+    
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
