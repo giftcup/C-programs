afd109aade0d21aef8e23490ba22336ef92c0d4e
giftcup <salometambe@gmail.com>
2021-07-17 23:13:53 +0100

    Revised the code to geberate the reverse binary number from the decimal

--- binary/forming.c
+++ binary/forming.c
@@ -4,21 +4,23 @@
 /* Version 17. 07. 21 */
 /* by ~giftcup */
 
-int count = 0;
-int placeholder = 0;
-int binary = 0;
+int forming(int decimal, int* tens){
+    int count = 0;
+    int placeholder = 0;
+    int binary = 0;
 
-while(decimal != 0) {
-        binary = decimal % 2;
-        decimal /= 2;
-        if(count != 0) {
-            *tens *= 10;
-            placeholder = (placeholder * 10) + binary;
+    while(decimal != 0) {
+            binary = decimal % 2;
+            decimal /= 2;
+            if(count != 0) {
+                *tens *= 10;
+                placeholder = (placeholder * 10) + binary;
+            }
+            else {
+                placeholder = binary;
         }
-        else {
-            placeholder = binary;
-	}
-        count ++;
-}
+            count ++;
+    }
 
-return placeholder;
+    return placeholder;
+}
