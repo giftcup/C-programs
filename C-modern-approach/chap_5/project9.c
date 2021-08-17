a43b26b2ee1c6bc0ec0afda1e23eb17da94539c4
giftcup <salometambe@gmail.com>
2021-08-17 23:03:58 +0100

    Corrected an error

--- C-modern-approach/chap_5/project9.c
+++ C-modern-approach/chap_5/project9.c
@@ -12,15 +12,18 @@ int main(void)
     int first, sec;
 
     printf("Enter date (mm/dd/yy): ");
-    scanf("%d/%d/%d", &firstmon, &firstday, &firstmon);
+    scanf("%d/%d/%d", &firstmon, &firstday, &firstyr);
     printf("Enter date (mm/dd/yy): ");
     scanf("%d/%d/%d", &secmon, &secday, &secyr);
 
-    first = firstday + firstmon * DAYS_IN_MONTH + firstyr * DAYS_IN_YEAR;
-    sec = secday + secmon * DAYS_IN_MONTH + secyr * DAYS_IN_YEAR;
+    first = firstday + (firstmon * DAYS_IN_MONTH) + (firstyr * DAYS_IN_YEAR);
+    sec = secday + (secmon * DAYS_IN_MONTH) + (secyr * DAYS_IN_YEAR);
     
-    if (first < sec) printf("%.2d/%.2d/%.2d is earlier\n", firstmon, firstday, firstyr);
-    else if (sec < first) printf("%.2d/%.2d/%.2d is earlier\n", secmon, secday, secyr);
-    else printf("The dates are the same.\n");
+    if (first < sec) 
+        printf("%.2d/%.2d/%.2d is earlier\n", firstmon, firstday, firstyr);
+    else if (sec < first) 
+        printf("%.2d/%.2d/%.2d is earlier\n", secmon, secday, secyr);
+    else 
+        printf("The dates are the same.\n");
 
 }
