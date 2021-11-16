0b3d68f9fb7595bcb65f3e1adee646093df67653
giftcup <salometambe@gmail.com>
2021-11-16 20:53:10 +0100

    Modified reverse array to use functions

--- reverse.c
+++ reverse.c
@@ -1,27 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int *reverse_array(int size, int *arr);
+
 int main()
 {
     int num, *arr, i;
+    printf("Enter size of array: ");
     scanf("%d", &num);
     arr = (int*) malloc(num * sizeof(int));
+    printf("Enter elements in array: ");
     for(i = 0; i < num; i++) {
         scanf("%d", arr + i);
     }
-    int *temp = 0;
-    temp = (int*)malloc(num * sizeof(int));
-    for (int j = 0; j < num; j++) {
-        *(temp + j) = *(arr + j);
-    }
-    
-    // logic to reverse the array.
- 
-    for(int i = 0; i < num; i++){
-        *(arr + i) = *(temp + num - i - 1);
-    }
-    
+    int *reversed_array = (int*)malloc(sizeof(int) * num);
+    reversed_array = reverse_array(num, arr);
+    printf("The reversed arr is: ");
     for(i = 0; i < num; i++)
-        printf("%d ", *(arr + i));
+        printf("%d ", *(reversed_array + i));
+    putchar('\n');
     return 0;
+}
+
+int *reverse_array(int size, int *arr)
+{
+    for (int i = 0, j = size - 1; i < size/2 && j >= 0; i++, j--)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
 }
